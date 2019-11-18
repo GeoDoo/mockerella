220ff54918ec3f865175c29cd4665c751b42874a
Georgios Karametas <geometas@hotmail.com>
2019-11-18 22:10:28 +0000

    Mock posts request hardcodedly

--- features/support/interceptor.js
+++ features/support/interceptor.js
@@ -1,3 +1,36 @@
+const posts = [
+  {
+    title: { rendered: "How to properly test a React component: an example" }
+  },
+  {
+    title: { rendered: "Refactoring a generator" }
+  },
+  {
+    title: { rendered: "Defensive coding" }
+  },
+  {
+    title: { rendered: "GeodooBlog: new start!" }
+  },
+  {
+    title: { rendered: "Mid-year resolutions check" }
+  },
+  {
+    title: { rendered: "Introduction to Reactive programming: part 2" }
+  },
+  {
+    title: { rendered: "JS exponential operator" }
+  },
+  {
+    title: { rendered: "Introduction to Reactive programming: part 1" }
+  },
+  {
+    title: { rendered: "Memoization: a use case" }
+  },
+  {
+    title: { rendered: "A night to remember! NPM total meltdown" }
+  }
+];
+
 module.exports = {
   startOn: async function(page, requestsToMock, logger) {
     await page.setRequestInterception(true);
@@ -10,7 +43,14 @@ module.exports = {
         return;
       }
 
-      interceptedRequest.continue();
+      if (interceptedRequest.url().endsWith("posts")) {
+        interceptedRequest.respond({
+          status: 200,
+          contentType: "application/json; charset=UTF-8",
+          body: JSON.stringify(posts)
+        });
+      } else interceptedRequest.continue();
+
       logger.record(interceptedRequest, requestsToMock);
     });
   }
